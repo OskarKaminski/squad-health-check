f2952be5c347722741e383ab88f9d496faaf702e
Oskar Kaminski <oskarkaminski.dev@gmail.com>
2018-10-02 15:16:14 +0200

    Integration with cognito

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Routing from './src/config/loginRouting'
+import Routing from './src/config/appRouting'
+import Amplify from 'aws-amplify';
+import { withAuthenticator } from 'aws-amplify-react-native';
+import aws_exports from './src/aws-exports';
+Amplify.configure(aws_exports);
 
-export default class App extends React.Component {
+class App extends React.Component {
   render() {
     return <Routing />
   }
@@ -16,3 +20,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default withAuthenticator(App)
--- src/screens/History.screen.js
+++ src/screens/History.screen.js
@@ -1,7 +1,5 @@
 import React from 'react'
-import {View, Text, Image, FlatList} from 'react-native'
-import styles from './History.styles'
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
+import {View, Text, Image, FlatList, Dimensions} from 'react-native'
 
 export default class History extends React.Component {
     state = {
@@ -17,37 +15,37 @@ export default class History extends React.Component {
                 img: 'http://assets.stickpng.com/thumbs/580b585b2edbce24c47b2875.png'
             },
             {
-                id: 'Learning',
+                id: 'learning',
                 label: 'Learning',
                 img: 'http://assets.stickpng.com/thumbs/580b585b2edbce24c47b2772.png',
             },
             {
-                id: 'Speed',
+                id: 'speed',
                 label: 'Speed',
                 img: 'http://www.freeiconspng.com/uploads/running-man-icon-0.png',
             },
             {
-                id: 'Progress',
+                id: 'progress',
                 label: 'Progress',
                 img: 'http://pluspng.com/img-png/loader-png--194.png',
             },
             {
-                id: 'Fun',
+                id: 'fun',
                 label: 'Fun',
                 img: 'http://assets.stickpng.com/thumbs/5a340185f2aaf7518bd292c9.png',
             },
             {
-                id: 'Support',
+                id: 'support',
                 label: 'Support',
                 img: 'https://img.clipartxtras.com/2d69acf45a6efc2305640c073821a808_learning-support-resource-teaching-team-ballymitty-national-school-support-clipart-png_2220-2220.png',
             },
             {
-                id: 'TeamWork',
+                id: 'teamWork',
                 label: 'TeamWork',
                 img: 'http://www.freeiconspng.com/uploads/team-icon-3.png',
             },
             {
-                id: 'Mission',
+                id: 'mission',
                 label: 'Mission',
                 img: 'http://www.pngall.com/wp-content/uploads/2016/05/Mission-PNG-Picture.png',
             },
@@ -57,104 +55,81 @@ export default class History extends React.Component {
                 id: 1,
                 label: 'Retro #1',
                 results: {
-                    codebaseHealth: 10,
-                    delivery: 85,
-                    TeamWork: 10
+                    codebaseHealth: 10
                 }
-            }, {
+            },
+            {
                 id: 2,
                 label: 'Retro #2',
                 results: {
-                    codebaseHealth: 60,
-                    delivery: 40,
-                    TeamWork: 90
+                    codebaseHealth: 70
+                }
+            },
+            {
+                id: 3,
+                label: 'Retro #3',
+                results: {
+                    codebaseHealth: 35
                 }
             },
         ]
     }
 
-    render() {
+    componentWillMount () {
+        this.width = Dimensions.get('window').width
+    }
+
+    buildPath (results) {
+        if (results.length === 1) {
+            return `M0 ${results[0]} H100`
+        }
+
+        const moveX = 100 / (results.length - 1)
+        const entry = `M0 ${results.shift()}`
+        return results.reduce((prev, curr, i) => {
+            return prev + `L${i + 1 * moveX} ${curr}`
+        }, entry)
+
+    }
+
+    item = ({item}) => {
         return (
-            <View>
-                <FlatList
-                    data={this.state.categories}
-                    renderItem={({item}) => (
-                        <View style={{
-                            flexDirection: 'row',
-                            alignItems: 'center',
-                            borderBottomWidth: 1,
-                            borderColor: 'rgba(0,0,0,.3)',
-                            padding: 5,
-                            paddingLeft: 15
-                        }}>
-                            <View style={{alignItems: 'center', width: '20%'}}>
-                                <Image style={{width: 40, height: 40}}
-                                       source={{url: item.img}}
-                                />
-                                <Text>{item.label}</Text>
-                            </View>
-                            <View>
-                                <Text>{this.state.retros[this.state.retros.length - 1].results[item.id]}</Text>
-                            </View>
-                            <View style={{marginLeft: 10}}>
-                                {
-                                    this.state.retros[this.state.retros.length - 1].results[item.id] > this.state.retros[this.state.retros.length - 2].results[item.id] ?
-                                        <Icon name="thumb-up" color="black" size={16}/> :
-                                        this.state.retros[this.state.retros.length - 1].results[item.id] === this.state.retros[this.state.retros.length - 2].results[item.id] ?
-                                            null :
-                                            <Icon name="thumb-down" color="black" size={16}/>
-                                }
-                            </View>
-                        </View>
-
-                    )}
-                />
-                <View style={styles.table}>
-
-                    <View style={[styles.healthItem, styles.label]}>
-                        <View style={styles.healthItemCell}>
-                            <Text></Text>
-                        </View>
-                        <View style={styles.healthItemCell}>
-                            <Text>Sprint</Text>
-                        </View>
-                        {
-                            this.state.retros.map(retro => (
-                                <View key={retro.id} style={styles.healthItemCell}>
-                                    <Text>{retro.label}</Text>
-                                </View>
-                            ))
-                        }
-                    </View>
+            <View
+                style={{
+                    borderBottomWidth: 1,
+                    borderColor: 'rgba(0,0,0,.3)',
+                    padding: 5,
+                    paddingLeft: 15
+                }}>
+                <View style={{alignItems: 'center', flex: 1}}>
+                    <Image style={{width: 40, height: 40}}
+                           source={{url: item.img}}
+                    />
+                    <Text>{item.label}</Text>
+                </View>
+                <View style={{flexDirection: 'row', justifyContent: 'space-around', flex: 1}}>
                     {
-                        this.state.categories.map(category => (
-                            <View key={category.id} style={styles.healthItem}>
-                                <View style={[styles.healthItemCell, styles.label]}>
-                                    <Image source={{uri: category.img}} style={styles.categoryImage}/>
-                                </View>
-                                <View style={[styles.healthItemCell, styles.label]}>
-                                    <Text>{category.label}</Text>
-                                </View>
-                                {
-                                    this.state.retros.map((retro, i) => (
-                                        <View key={retro.id} style={[styles.healthItemCell, styles.resultCell]}>
-                                            <Text style={{marginRight: 5}}>{retro.results[category.id]}</Text>
-                                            {
-                                                this.state.retros[i - 1] && this.state.retros[i - 1].results[category.id] && this.state.retros[i - 1].results[category.id] < retro.results[category.id] &&
-                                                <Icon name="thumb-up" color="black" size={16}/>
-                                            }
-                                            {
-                                                this.state.retros[i - 1] && this.state.retros[i - 1].results[category.id] && this.state.retros[i - 1].results[category.id] > retro.results[category.id] &&
-                                                <Icon name="thumb-down" color="black" size={16}/>
-                                            }
-
-                                        </View>
-                                    ))
-                                }
-                            </View>
+                        this.state.retros.map(({results}, i) => (
+                            <Text style={{
+                                marginTop: 50 - (results[item.id] / 2)
+                            }}>
+                                {results[item.id]}
+                            </Text>
                         ))
                     }
                 </View>
+            </View>
+        )
+    }
+
+    render () {
+        return (
+            <View style={{marginTop: 25}}>
+                <FlatList
+                    data={this.state.categories}
+                    keyExtractor={item => item.id}
+                    renderItem={this.item}
+                />
             </View>
         )
     }
