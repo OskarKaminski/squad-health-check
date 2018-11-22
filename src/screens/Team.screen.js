74864634c09c069ceda1a00a9d9abbc5f8de1c1a
Oskar Kaminski <oskarkaminski.dev@gmail.com>
2018-11-22 20:50:41 +0100

    Team go back functionality

--- src/screens/Team.screen.js
+++ src/screens/Team.screen.js
@@ -8,21 +8,35 @@ import {getGroup} from '../graphql/queries';
 
 const Page = styled.View`
     flex: 1;
-    backgroundColor: white;
-    justifyContent: space-between;
+    background-color: white;
+    justify-content: space-between;
 `
 
 const Header = styled.View`
     backgroundColor: #0CAADC;
     height: 150px;
-    justifyContent: center;
+    flex-direction: row;
+    justify-content: space-between;
     align-items: center;
 `
 
-const HeaderText = styled.Text`
+const HeaderCenter = styled.Text`
   color: #fff;
   font-size: 20px;
   font-weight: bold;
+  text-align: center;
+  flex: 3;
+`
+
+const HeaderRight = styled.Text`
+  flex: 1;
+  align-items: center;
+`
+
+const HeaderLeft = styled.TouchableOpacity`
+  flex: 1;
+  align-items: center;
+  padding: 10px 0;
 `
 
 
@@ -35,6 +49,7 @@ export default class Team extends React.Component {
     }
 
     render () {
+
         return (
             <Connect
                 query={graphqlOperation(getGroup, {id: this.props.navigation.state.params.id})}
@@ -49,7 +64,11 @@ export default class Team extends React.Component {
                         <Page>
                             <Content>
                                 <Header>
-                                    <HeaderText>{data.getGroup.description}</HeaderText>
+                                    <HeaderLeft onPress={() => this.props.navigation.goBack()}>
+                                        <Icon name='ios-arrow-back' type='Ionicons' style={{color: '#fff', fontSize: 30}} />
+                                    </HeaderLeft>
+                                    <HeaderCenter>{data.getGroup.description}</HeaderCenter>
+                                    <HeaderRight />
                                 </Header>
                                 <Text>Hey</Text>
                             </Content>
